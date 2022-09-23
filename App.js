d1c474c0f86f54847fdf044b74eae2d66271eb5a
Sam Johnson <sambjohnson94@gmail.com>
2022-09-22 18:39:51 -0700

    update main css, add jsx rewrite of index.html

---
+++ App.js
@@ -0,0 +1,334 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta content="IE=edge" http-equiv="X-UA-Compatible">
+    <meta content="width=device-width,initial-scale=1" name="viewport">
+    <meta content="description" name="description">
+    <meta name="google" content="notranslate" />
+    <meta content="Mashup templates have been developped by Orson.io team" name="author">
+
+    <!-- Disable tap highlight on IE -->
+    <meta name="msapplication-tap-highlight" content="no">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="./assets/apple-icon-180x180.png">
+    <link href="./assets/favicon.ico" rel="icon">
+
+    <title>Sam Johnson Developer Portfolio</title>
+
+    <link href="./main.d8e0d294.css" rel="stylesheet"></head>
+
+    <body class="">
+
+      <!-- Add your content of header -->
+      <div class="background-color-layer" style="background-image: url('assets/images/headshot.jpg')"
+      ></div>
+      <main class="content-wrapper">
+        <header class="white-text-container section-container">
+          <div class="text-center">
+            <h1>Sam Johnson (she/her)</h1>
+            <p>Polyglot Developer, Data Scientist, Bioinformatician</p>
+            <p><small>Location: Berkeley, California</small></p>
+            <p>
+              <a class="fa-icon fa-icon-2x" href="https://www.github.com/Pinjontall94" title="">
+                <i class="fa fa-github"></i>
+              </a>
+              <a class="fa-icon fa-icon-2x" href="https://www.gitlab.com/Pinjontall94" title="">
+                <i class="fa fa-gitlab"></i>
+              </a>
+              <a class="fa-icon fa-icon-2x" href="https://www.linkedin.com/samuelbjohnson94" title="">
+                <i class="fa fa-linkedin"></i>
+              </a>
+              <a class="fa-icon fa-icon-2x" href="mailto:sambjohnson94@gmail.com" title="">
+                <i class="fa fa-envelope"></i>
+              </a>
+              <a class="fa-icon fa-icon-2x" href="tel:16616443857" title="">
+                <i class="fa fa-phone"></i>
+              </a>
+            </p>
+          </div>
+        </header>
+
+
+
+        <!-- Add your site or app content here -->
+
+        <div class="container">
+          <div class="row">
+            <div class="col-xs-12">
+
+              <div class="card">
+                <div class="card-block">
+                  <h2>About me</h2>
+                  <div class="row">
+                    <div class="col-md-4">
+                      <p><img src="./assets/images/headshot.jpg" class="img-responsive" alt=""></p>
+                    </div>
+                    <div class="col-md-8">
+
+                      <p>
+                        Experienced developer with 4 years of experience in
+                        scientific Python (Pandas), R, and Unix shell, and over
+                        7 years in biological research. Fluent in the
+                        aforementioned technologies, and experienced in
+                        fullstack development in Javascript, Nodejs, React,
+                        Elixir, and Common Lisp. Strong foundation in scalable
+                        functional programming.
+                      </p>
+
+                      <p>
+                        Highly adaptable professional with a B.S. in Plant
+                        Sciences from the University of California, Davis.
+                        Well-versed in PCR, DNA/RNA extraction, gas
+                        chromatography, microscopy, and spectrophotometry.
+                        Highly experienced in cell culture, hemocytometry, and
+                        aseptic technique.
+                      </p>
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="card">
+                <div class="card-block">
+                  <h2>Work</h2>
+                  <div class="work-experience">
+                    <small class="date">2019-Present</small>
+                    <h3 class="h5 date-title">
+                      Bioinformatics Researcher -
+                      <a href="https://www.ars.usda.gov/" title="U.S. Department of Agriculture">USDA-ARS</a>
+                    </h3>
+                    <p>
+                      <ul>
+                        <li>
+                          Create, test, and debug data pipelines for
+                          metagenomics research; currently focused on studies of
+                          agricultural management practices to control
+                          soil-borne plant pathogens
+                        </li>
+                        <li>
+                          Develop unix shell scripts (bash), R, and Python
+                          programs on the UCD Genome Center Cluster
+                        </li>
+                        <li>
+                          Learned and integrated the following bioinformatics
+                          packages: sra-tools, dada2, vsearch, snakemake, QIIME2
+                        </li>
+                      </ul>
+                    </p>
+                  </div>
+
+                  <div class="work-experience">
+                    <small class="date">2021-2022</small>
+                    <h3 class="h5 date-title">
+                      Research Assistant -
+                      <a href="https://innerplant.com" title="">
+                        Innerplant
+                      </a>
+                    </h3>
+
+                    <p>
+                      <ul>
+                        <li>
+                          Ran trials on transformed crop plants, tested
+                          downstream vectors for fluorescent protein expression
+                          and induction
+                        </li>
+                        <li>
+                          Maintained and upgraded the embedded camera's software
+                          stack (incl. Basler Pylon Python API, PyQt5, OpenCV,
+                          Pillow/PIL)
+                        </li>
+                        <li>
+                          Fixed video orientation & scaling, added a linear
+                          brightness view mode as well as modern version control
+                          with git
+                        </li>
+                      </ul>
+                    </p>
+                  </div>
+
+                  <div class="work-experience">
+                    <small class="date">2021-Present</small>
+                    <h3 class="h5 date-title">
+                      Co-Founder & System Administrator -
+                      <a href="https://virtualgalleryonline.org" title="">
+                        Virtual Gallery Online
+                      </a>
+                    </h3>
+
+                    <p>
+                      <ul>
+                        <li>
+                          Interface with Google Cloud Platform to provide VPS
+                          hosting
+                        </li>
+                        <li>
+                          Configured Apache2, DNS settings, and TLS
+                          certification with EFF Certbot/Let' s Encrypt
+                        </li>
+                        <li>
+                          Installed Wordpress and integrated the site with the
+                          VPS' MySQL database
+                        </li>
+                        <li>
+                          Perform Debian Linux system administration to keep the
+                          site in top working order for the Wordpress frontend
+                        </li>
+                      </ul>
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="card">
+                <div class="card-block">
+                  <h2>Projects</h2>
+                  <div class="row">
+                    <div class="col-md-4">
+                      <img src="./assets/images/16s-rRNA.png" class="img-responsive" alt="">
+                      <h3 class="h5">ASD-Q2</h3>
+                      <p>May 2019</p>
+                    </div>
+                    <div class="col-md-4">
+                      <img src="./assets/images/ros-create.png" class="img-responsive" alt="">
+                      <h3 class="h5">Ros Create</h3>
+                      <p>Jun 2022</p>
+                    </div>
+                    <div class="col-md-4">
+                      <img src="./assets/images/depo-estradiol.jpeg" class="img-responsive" alt="">
+                      <h3 class="h5">Tranalytics</h3>
+                      <p>Jun 2022</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+
+              <div class="card">
+                <div class="card-block">
+                  <h2>Education</h2>
+                  <div class="row">
+                    <div class="col-md-4">
+                      <div class="education-experience">
+                        <small class="date">2012-2016</small>
+                        <h3 class="h5 date-title">B.S. in Plant Sciences</h3>
+                        <p>University of California, Davis</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="card">
+                <div class="card-block">
+                  <h2>Language</h2>
+                  <div class="row">
+                    <div class="col-md-4">
+                      <div class="language-experience">
+                        <h3 class="h5">English  <small>Native</small></h3>
+                      </div>
+                    </div>
+                    <div class="col-md-4">
+                      <div class="language-experience">
+                        <h3 class="h5">Chinese (中文) <small>Intermediate</small></h3>
+                      </div>
+                    </div>
+                    <div class="col-md-4">
+                      <div class="language-experience">
+                        <h3 class="h5">Swedish  <small>Intermediate</small></h3>
+                      </div>
+                    </div>
+                    <div class="col-md-4">
+                      <div class="language-experience">
+                        <h3 class="h5">Spanish  <small>Beginner</small></h3>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="card">
+                <div class="card-block">
+                  <h2>Social Network</h2>
+                  <div class="row">
+                    <div class="col-md-3">
+                      <p class="social-buttons">
+                        <a href="https://github.com/Pinjontall94" title="">
+                          <span class="social-round-icon fa-icon"><i class="fa fa-github"></i></span>Github
+                        </a>
+                      </p>
+                    </div>
+                    <div class="col-md-3">
+                      <p class="social-buttons">
+                        <a href="https://gitlab.com/Pinjontall94" title="">
+                          <span class="social-round-icon fa-icon"><i class="fa fa-gitlab"></i></span>Gitlab
+                        </a>
+                      </p>
+                    </div>
+                    <div class="col-md-3">
+                      <p class="social-buttons">
+                        <a href="https://www.linkedin.com/samuelbjohnson94" title="">
+                          <span class="social-round-icon fa-icon"><i class="fa fa-linkedin"></i></span>LinkedIn
+                        </a>
+                      </p>
+                    </div>
+                    <div class="col-md-3">
+                      <p class="social-buttons">
+                        <a href="mailto:sambjohnson94@gmail.com" title="">
+                          <span class="social-round-icon fa-icon"><i class="fa fa-envelope"></i></span>Send email
+                        </a>
+                      </p>
+                    </div>
+                    <div class="col-md-3">
+                      <p class="social-buttons">
+                        <a href="tel:16616443857" title="">
+                          <span class="social-round-icon fa-icon"><i class="fa fa-phone"></i></span>Call
+                        </a>
+                      </p>
+                    </div>
+
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+      </main>
+      <footer class="footer-container white-text-container text-center">
+        <div class="container">
+          <div class="row">
+            <div class="col-xs-12">
+              <p><img src="./assets/images/mashup-icon.svg" alt=""></p>
+
+              <p>©All right reserved. Design <a href="http://www.mashup-template.com/" title="Create website with free html template">Mashup Template</a>/<a href="https://unsplash.com/" title="Beautiful Free Images">Unsplash</a></p>
+            </div>
+          </div>
+        </div>
+      </footer>
+
+      <script>
+       document.addEventListener("DOMContentLoaded", function (event) {
+         scrollRevelation('.card');
+       });
+      </script>
+      <!-- Google Analytics: change UA-XXXXX-X to be your site's ID
+
+           <script>
+           (function (i, s, o, g, r, a, m) {
+           i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+           (i[r].q = i[r].q || []).push(arguments)
+           }, i[r].l = 1 * new Date(); a = s.createElement(o),
+           m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+           })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+           ga('create', 'UA-XXXXX-X', 'auto');
+           ga('send', 'pageview');
+           </script>
+
+      --><script type="text/javascript" src="./main.bc58148c.js"></script></body>
+
+</html>
